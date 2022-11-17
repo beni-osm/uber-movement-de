169f83c5db0012fee02bef186b04045581fc40d5
beni-osm <71977151+beni-osm@users.noreply.github.com>
2022-11-17 09:41:58 +0100

    Update uber_gluejob.py
    Correction on creating folder in bucket.

--- src/uber_gluejob.py
+++ src/uber_gluejob.py
@@ -1,6 +1,4 @@
-# AWS Libraries
 import sys
-import boto3
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -10,7 +8,8 @@ from awsglue.dynamicframe import DynamicFrame
 
 from datetime import datetime
 
-# AWS Config
+import boto3
+
 client = boto3.client('s3')
 
 ## @params: [JOB_NAME]
@@ -22,36 +21,39 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Data config
-DATABASE = "uber-movement-data"
-TABLE_NAME = "uber_movement_raw_data"
-
-
 def create_folder():
     response = client.put_object(
         Bucket='uber-movement-processed-data',
         Body='',
         Key=f'{processed_folder}/'
     )
+date_time = datetime.now()
+processed_folder = str(date_time.year) + '_' + str(date_time.month) + '_' + str(date_time.day) + '_' + str(
+    date_time.hour)
 
+# Check if the folder exists
+result = client.list_objects_v2(Bucket='uber-movement-processed-data', Prefix=processed_folder)
 
-create_folder()
+if 'Contents' in result:
+    print("Key exists in the bucket.")
+else:
+    print("Key doesn't exist in the bucket.")
+    create_folder()
 
-uber_data = glueContext.create_dynamic_frame.from_catalog(database=DATABASE,
-                                                          table_name=TABLE_NAME,
-                                                          transformation_ctx="datasource0").toDF()
+# Reading data from glue catalog
+uber_data = glueContext.create_dynamic_frame.from_catalog(database = "uber-movement-data", table_name = "uber_movement_raw_data", transformation_ctx = "datasource0").toDF()
+
+# Using relevant columns
 uber_data = uber_data.select('Origin Movement ID', 'Origin Display Name', 'Origin Geometry',
                              'Destination Movement ID', 'Destination Display Name',
                              'Destination Geometry', 'Date Range', 'Mean Travel Time (Seconds)',
                              'Range - Lower Bound Travel Time (Seconds)',
                              'Range - Upper Bound Travel Time (Seconds)')
-date_time = datetime.now()
-processed_folder = str(date_time.year) + '_' + str(date_time.month) + '_' + str(date_time.day) + '_' + str(
-    date_time.hour)
+
+# Converting dataframe to DynamicFrame
 uber_movement_frame = DynamicFrame.fromDF(uber_data, glueContext, "uber_movement_data")
 
-uber_data_sink = glueContext.write_dynamic_frame.from_options(frame=uber_movement_frame, connection_type="s3",
-                                                              connection_options={
-                                                                  "path": f"s3://uber-movement-processed-data/{processed_folder}/"},
-                                                              format="parquet", transformation_ctx="uber_data_sink")
+# Write the new version of the data in processed zone.
+uber_data_sink = glueContext.write_dynamic_frame.from_options(frame = uber_movement_frame, connection_type = "s3", 
+                                                        connection_options = {"path": f"s3://uber-movement-processed-data/{processed_folder}/"}, format = "parquet", transformation_ctx = "uber_data_sink")
 job.commit()
